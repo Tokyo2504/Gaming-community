539f246657d6d93b39316954b35e850a546bad3c
Tokyo2504 <ksu34vol@gmail.com>
2022-07-01 22:30:27 +0500

    updated

--- src/js/components/games-swipper.js
+++ src/js/components/games-swipper.js
@@ -1,28 +1,21 @@
-import Swiper, { Navigation } from 'swiper';
- Swiper.use([Navigation]);
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
+ Swiper.use([Navigation, Pagination, Autoplay]);
 
 const gamesSlider = document.querySelector('.games__slider');
-const gamesSlider1 = document.querySelector('.games__slider1');
 
 let slider = new Swiper(gamesSlider, {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  grabCursor: true,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    }
-  }
-});
-
-let slider1 = new Swiper(gamesSlider1, {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  grabCursor: true,
-  initialSlide: 4,
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-    }
-  }
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+  autoplay: {
+    delay: 3000,
+   },
+  navigation: {
+    nextEl: '.games-button-next',
+    prevEl: '.games-button-prev',
+  },
+  pagination: {
+    el: '.games__pag',
+    type: 'bullets',
+    clickable: true,
+  },
 });
